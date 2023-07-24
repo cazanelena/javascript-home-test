f2813b89f4785188f0be76a8101110bcef50d561
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-24 17:35:37 +0100

    tests added

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,50 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+  if (typeof expect !== typeof actual) {
+    throw new Error(
+      `Expected type ${typeof expect} but got type ${typeof actual}`
+    );
+  }
 
-module.exports = assertEquals
+  if (typeof expect === "object") {
+    if (Array.isArray(actual)) {
+      assertArrayEquals(expect, actual);
+    } else {
+      // TODO: do object comparison
+      // if the objects are the same not to throw
+      throw new Error("object comparison is not supported");
+    }
+  }
+
+  // do assertion for simple types, boolean, number ...
+
+  if (typeof expect === "string") {
+    if (expect !== actual) {
+      throw new Error(`Expected ${expect} to be same as ${actual}`);
+    }
+  }
+  if (typeof expect === "boolean") {
+    if (expect !== actual) {
+      throw new Error(`Exected ${expect} to be same as ${actual}`);
+    }
+  }
+  // TO DO assert equals for undefined, numbers
+  if (typeof expect === "number") {
+    if (expect !== actual) {
+      throw new Error(`Expected ${expect} to be same as ${actual}`);
+    }
+  }
+}
+
+function assertArrayEquals(expect, actual) {
+  if (expect.length !== actual.length) {
+    throw new Error(
+      `Expected both arguments to have the same length but received ${expect.length} and ${actual.length}`
+    );
+  }
+
+  for (let i = 0; i < expect.length; i++) {
+    assertEquals(expect[i], actual[i]);
+  }
+}
+
+module.exports = assertEquals;
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,73 @@
-const assertEquals = require('./assert-equals')
+const assertEquals = require("./assert-equals");
 
-describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+describe("assertEquals", () => {
+  it("not the same type", () => {
+    expect(() => assertEquals(1, "1")).toThrow();
+  });
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+  it("returns without throwing an error", () => {
+    expect(() => assertEquals("abc", "abc")).not.toThrow();
+  });
+
+  it("not the same string", () => {
+    expect(() => assertEquals("abc", "acb")).toThrow();
+  });
+
+  it("not the same string length", () => {
+    expect(() => assertEquals("abc", "abcc")).toThrow();
+  });
+
+  it("does not throw on error on comparing flat arrays", () => {
+    expect(() => assertEquals(["a", "b", "c"], ["a", "b", "c"])).not.toThrow();
+  });
+
+  it("throws an error for array of different length", () => {
+    expect(() => assertEquals(["a", "b"], ["a", "b", "c"])).toThrow();
+  });
+
+  it("throws an error for arrays with different elements", () => {
+    expect(() => assertEquals(["a", "b"], ["a", "d"])).toThrow();
+  });
+
+  it("does not throw on error on comparing nested arrays", () => {
+    expect(() =>
+      assertEquals([["a"], ["b", "c"]], [["a"], ["b", "c"]])
+    ).not.toThrow();
+  });
+
+  it("handles the counter example against flattened arrays", () => {
+    expect(() =>
+      assertEquals([["a", "b"], ["c"]], [["a"], ["b", "c"]])
+    ).toThrow();
+  });
+
+  it("can compare arrays with different element types", () => {
+    expect(() => assertEquals([1, "9"], [1, "9"])).not.toThrow();
+  });
+
+  it("throws when comparing different basic types", () => {
+    expect(() => assertEquals(1, "1")).toThrow();
+  });
+
+  it("throws when comparing a basic type to a complex type", () => {
+    expect(() => assertEquals([1], 1)).toThrow();
+  });
+
+  it("throws when comparing two objects", () => {
+    expect(() =>
+      assertEquals({ 1: "test" }, { 1: "test1", 2: "test2" })
+    ).toThrow();
+  });
+  it("throws when two bool have different values", () => {
+    expect(() => assertEquals(true, false)).toThrow();
+  });
+  it("does not throw an error when both bool have the same values", () => {
+    expect(() => assertEquals(true, true)).not.toThrow();
+  });
+  it("throws when both elemants are numbers and they have different values", () => {
+    expect(() => assertEquals(2, 3)).toThrow();
+  });
+  it("does not throw an error when both elements are numbers and have same value", () => {
+    expect(() => assertEquals(2, 2)).not.toThrow();
+  });
+});
